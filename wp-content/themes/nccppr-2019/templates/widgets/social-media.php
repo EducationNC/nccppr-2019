3e11230284590e08c955dd6d0bf287d5c1e6bcef
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-08-16 11:46:28 -0400

    social

--- wp-content/themes/nccppr-2019/templates/widgets/social-media.php
+++ wp-content/themes/nccppr-2019/templates/widgets/social-media.php
@@ -1,7 +1,33 @@
+<!-- <div class="container juicer">
+  <script src="//assets.juicer.io/embed.js" type="text/javascript"></script>
+  <link href="//assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
+  <ul class="juicer-feed" data-feed-id="educationnc"><h1 class="referral"><a href="http://www.juicer.io">Powered by Juicer</a></h1></ul>
+</div>
+
+<div class="follow-buttons text-center">
+  <h3>See more and follow EdNC on:</h3>
+  <a href="https://www.facebook.com/educationnc" target="_blank"><span class="icon-facebook"></span></a>
+  <a href="https://www.twitter.com/educationnc" target="_blank"><span class="icon-twitter"></span></a>
+  <a href="https://www.instagram.com/educationnc" target="_blank"><span class="icon-instagram"></span></a>
+</div> -->
+
 <div class="container">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="fb-page" data-href="https://www.facebook.com/nccppr" data-width="400" data-height="500" data-hide-cover="false" data-show-facepile="true" data-show-posts="true" data-small-header="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/nccppr"><a href="https://www.facebook.com/nccppr">NCCPPR</a></blockquote></div></div>
-    </div>
-  </div>
+  <?php
+  // Use shortcode in a PHP file (outside the post editor).
+  echo do_shortcode( '' );
+
+  // Use shortcodes in form like Landing Page Template.
+  echo do_shortcode( '[custom-twitter-feeds screenname="nccppr" num=4 include="author,date,text,avatar,media"]' );
+
+  // Store the short code in a variable.
+  $var = do_shortcode( '' );
+  echo $var;
+  ?>
+
+</div>
+
+<div class="follow-buttons text-center">
+  <h3>See more and follow GRA on:</h3>
+  <a href="https://www.facebook.com/nccppr" target="_blank"><span class="icon-facebook"></span></a>
+  <a href="https://www.twitter.com/nccppr" target="_blank"><span class="icon-twitter"></span></a>
 </div>
