ec4238c258159441f0ee9b3a6389a6f350d09c71
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-08-30 11:22:52 -0400

    voices pg

--- wp-content/themes/nccppr-2019/page-voices.php
+++ wp-content/themes/nccppr-2019/page-voices.php
@@ -35,6 +35,59 @@
       <?php endwhile; endif; wp_reset_query(); ?>
     </div>
 
+
+
+    <div class="row">
+       <div class="col-xs-12">
+          <h3 class="section-header">Specialists</h3>
+       </div>
+    </div>
+
+    <div class="row specialists">
+       <div class="col-xs-12">
+          <div class="tab-content">
+             <div role="tabpanel" class="tab-pane active" id="">
+                <?php
+                   $args = array(
+                     'post_type' => 'bio',
+                     'posts_per_page' => -1,
+                     'order' => 'ASC',
+                     'orderby' => 'meta_value title',
+                     'meta_key' => 'last_name_to_sort_by',
+                     'tax_query' => array(
+                       array(
+                         'taxonomy' => 'author-type',
+                         'field' => 'slug',
+                         'terms' => 'specialist'
+                       ),
+                     )
+                   );
+
+                   $specialists = new WP_Query($args);
+
+                   if ($specialists->have_posts()) : while ($specialists->have_posts()) : $specialists->the_post();
+                     $user = get_field('user');
+                     ?>
+                <div class="col-sm-4 col-xs-6 block-person block-person-min-height">
+                   <div class="position-relative">
+                      <a class="mega-link" href="<?php echo get_author_posts_url($user['ID']); ?>"></a>
+                      <div class="row">
+                         <div class="col-sm-5">
+                            <div class="circle-image"><?php the_post_thumbnail('bio-headshot'); ?></a></div>
+                         </div>
+                         <div class="col-sm-7">
+                            <h4 class="post-title"><?php the_title(); ?></h4>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <?php endwhile; endif; wp_reset_query(); ?>
+             </div>
+          </div>
+      </div>
+    </div>
+
+
     <div class="row">
       <div class="col-xs-12">
         <h3 class="section-header">Contributors</h3>
@@ -44,14 +97,57 @@
     <div class="row contributors">
       <div class="col-xs-12">
         <ul class="nav nav-tabs" role="tablist">
-          <li role="presentation" class="active"><a href="#y2017" aria-controls="y2017" role="tab" data-toggle="tab">2018</a></li>
+          <li role="presentation" class="active"><a href="#y2019" aria-controls="y2019" role="tab" data-toggle="tab">2019</a></li>
+          <li role="presentation"><a href="#y2018" aria-controls="y2018" role="tab" data-toggle="tab">2018</a></li>
           <li role="presentation"><a href="#y2017" aria-controls="y2017" role="tab" data-toggle="tab">2017</a></li>
           <li role="presentation"><a href="#y2016" aria-controls="y2016" role="tab" data-toggle="tab">2016</a></li>
-	  <li role="presentation"><a href="#y2015" aria-controls="y2015" role="tab" data-toggle="tab">2015</a></li>
+	        <li role="presentation"><a href="#y2015" aria-controls="y2015" role="tab" data-toggle="tab">2015</a></li>
         </ul>
 
         <div class="tab-content">
-<div role="tabpanel" class="tab-pane active" id="y2018">
+          <div role="tabpanel" class="tab-pane active" id="y2019">
+            <?php
+            $args = array(
+              'post_type' => 'bio',
+              'posts_per_page' => -1,
+              'order' => 'ASC',
+              'orderby' => 'meta_value title',
+              'meta_key' => 'last_name_to_sort_by',
+              'tax_query' => array(
+                array(
+                  'taxonomy' => 'author-type',
+                  'field' => 'slug',
+                  'terms' => 'contributor'
+                ),
+                array(
+                  'taxonomy' => 'author-year',
+                  'field' => 'slug',
+                  'terms' => '2019'
+                )
+              )
+            );
+
+            $contributors = new WP_Query($args);
+
+            if ($contributors->have_posts()) : while ($contributors->have_posts()) : $contributors->the_post();
+              $user = get_field('user');
+              ?>
+
+              <div class="col-sm-4 col-xs-6 block-person">
+                <div class="position-relative">
+                  <a class="mega-link" href="<?php echo get_author_posts_url($user['ID']); ?>"></a>
+                  <div class="row">
+                    <div class="col-sm-5">
+                      <div class="circle-image"><?php the_post_thumbnail('bio-headshot'); ?></a></div>
+                    </div>
+                    <div class="col-sm-7"><h4 class="post-title"><?php the_title(); ?></h4></div>
+                  </div>
+                </div>
+              </div>
+
+            <?php endwhile; endif; wp_reset_query(); ?>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="y2018">
             <?php
             $args = array(
               'post_type' => 'bio',
@@ -73,7 +169,7 @@
               )
             );
 
- $contributors = new WP_Query($args);
+            $contributors = new WP_Query($args);
 
             if ($contributors->have_posts()) : while ($contributors->have_posts()) : $contributors->the_post();
               $user = get_field('user');
@@ -92,8 +188,8 @@
               </div>
 
             <?php endwhile; endif; wp_reset_query(); ?>
-          </div>         
- <div role="tabpanel" class="tab-pane" id="y2017">
+          </div>
+          <div role="tabpanel" class="tab-pane" id="y2017">
             <?php
             $args = array(
               'post_type' => 'bio',
@@ -157,7 +253,7 @@
                 )
               )
             );
-       
+
      $contributors = new WP_Query($args);
 
             if ($contributors->have_posts()) : while ($contributors->have_posts()) : $contributors->the_post();
